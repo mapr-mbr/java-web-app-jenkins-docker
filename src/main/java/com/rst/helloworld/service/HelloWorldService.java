6a55eb56a7125677106c65c36bc36d84aac8f27d
bharathmbr <113525016+bharathmbr@users.noreply.github.com>
2025-05-16 17:53:26 +0530

    Update HelloWorldService.java

--- src/main/java/com/rst/helloworld/service/HelloWorldService.java
+++ src/main/java/com/rst/helloworld/service/HelloWorldService.java
@@ -25,7 +25,7 @@ public class HelloWorldService {
 		if(StringUtils.isEmpty(name)){
 			return "Hello from MAPR_MBR Technologies";
 		}else{
-			return "Hello " + name;
+			return "Hello  mbr" + name;
 		}
 		
 	}
